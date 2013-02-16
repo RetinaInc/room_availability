81d65cfba528a527298e0063ffd4d477fff614f8
Giuseppe Correnti <correnti.g@gmail.com>
2013-02-16 21:59:11 +0000

    Pagination now shows 5 entries per page

--- app/assets/javascripts/availability.js
+++ app/assets/javascripts/availability.js
@@ -3,6 +3,8 @@ $(function() {
 	$('#availability_datatable').dataTable({
 		"sDom": "<'row'<'span6'l><'span6'f>r>t<'row'<'span6'i><'span6'p>>",
 		sPaginationType: "bootstrap",
+		aLengthMenu: [[5, 25, 50, -1], [5, 25, 50, "All"]],
+		iDisplayLength: 5,
 		bProcessing: true,
 		bJQueryUI: true
 	});
