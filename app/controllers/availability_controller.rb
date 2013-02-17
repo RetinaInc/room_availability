3e33b2622e45e909ae790d7851b2cde3bec2d8fb
Giuseppe Correnti <correnti.g@gmail.com>
2013-02-17 16:19:32 +0000

    Refining and refactoring controller and controller tests

--- app/controllers/availability_controller.rb
+++ app/controllers/availability_controller.rb
@@ -1,17 +1,29 @@
 class AvailabilityController < ApplicationController
 
   def index
+    @availability_query = AvailabilityQuery.new
   end
   
   def search
-    @available_rooms = Host.all.collect {  
-      |host| host.available_rooms_between(params[:start_date],
-                                          params[:end_date],
-                                          params[:guests])
-    }
+    @availability_query = AvailabilityQuery.new(params[:availability_query])
     respond_to do |format|
-      format.html { render :index } 
-      format.json { render :json => @available_rooms.to_json }
-    end    
+      if @availability_query.valid?
+        @available_rooms = get_available_rooms(@availability_query)
+        format.html { render :index } 
+        format.json { render :json => @available_rooms.to_json, status: :created }
+      else
+        format.html { render :index }
+        format.json { render json: @availability_query.errors, status: :unprocessable_entity }
+      end
+    end
   end
+
+private 
+
+  def get_available_rooms(query)
+    Host.all.collect { 
+      |host| host.available_rooms_between(query.start_date, query.end_date, query.guests.to_i)
+    }
+  end
+
 end
--- spec/controllers/availability_controller_spec.rb
+++ spec/controllers/availability_controller_spec.rb
@@ -14,10 +14,10 @@ describe AvailabilityController do
     it { should render_template(:index) }
  
     # Checking that all the necessary elements are present
-    it { page.should have_selector("input#start_date") }
-    it { page.should have_selector("input#end_date") }
-    it { page.should have_selector("input#guests") }
-    it { page.should have_selector("button[type='submit']") }
+    it { page.should have_selector("input#availability_query_start_date") }
+    it { page.should have_selector("input#availability_query_end_date") }
+    it { page.should have_selector("input#availability_query_guests") }
+    it { page.should have_selector(".btn[type='submit']") }
     
     it { should_not render_template(:table)  }
     
@@ -25,32 +25,53 @@ describe AvailabilityController do
 
   describe "GET 'search'" do
     before(:all) do
+      @date = Date.today
       @hosts = create_list(:host_with_rooms, 5, available_rooms: 2)
-      @expected = @hosts.collect{ |host| host.rooms.collect { |room| [room.id, 0] } }
+      # There are no bookings so all the rooms are available (guests_booked = 0)
+      @expected = @hosts.collect{ |host| host.rooms.collect { |room| [room.id, 0] } } 
     end
       
     after :all do
       @hosts.each { |h| h.destroy } 
     end
     
-    context "JSON response" do
+    context "JSON response with no errors" do
       before :each do
-        get 'search', :format => :json, :start_date => Date.today, :end_date => Date.today, :guests => 1
+        get 'search', :format => :json, :availability_query => { :start_date => @date, :end_date => @date, :guests => 1 }
       end
       
-      it { should respond_with(:success) }
+      it { should respond_with(:created) }
       it { should respond_with_content_type(/json/)}
       it { should assign_to(:available_rooms) }
             
-      it "should return the correct JSON" do 
+      it "should return the correct JSON format" do 
         body = JSON.parse(response.body)
         body.should == @expected
       end
     end
     
+    context "JSON response with errors" do
+      before :all do
+        @availability_query = AvailabilityQuery.new(:start_date => "", :end_date => "", :guests => "dsa")
+        @expected = @availability_query.errors unless @availability_query.valid?
+      end
+      
+      before {
+        get 'search', :format => :json, :availability_query => { :start_date => "", :end_date => "", :guests => 'dasdas' }
+      }
+      
+      it { should respond_with(:unprocessable_entity) }
+      it { should_not assign_to(:available_rooms) }      
+      it { should respond_with_content_type(/json/)}
+      
+      it "should return the errors in JSON format" do 
+        response.body.should == @expected.to_json
+      end
+    end
+    
     context "HTML response" do
       before :each do
-        get 'search', :format => :html, :start_date => Date.today, :end_date => Date.today, :guests => 1
+        get 'search', :format => :html, :availability_query => { :start_date => @date, :end_date => @date, :guests => 1 }
       end
       
       it { should render_template(:index) }
