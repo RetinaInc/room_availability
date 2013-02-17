c0c06cee84d3b7f37fb8512ddbb8e6c2021c5c85
Giuseppe Correnti <correnti.g@gmail.com>
2013-02-17 22:55:00 +0000

    Date validation for AvailabilityQuery
    * Format is 'yyyy-mm-dd'
    * Start date must precede end date

--- app/models/availability_query.rb
+++ app/models/availability_query.rb
@@ -4,9 +4,11 @@ class AvailabilityQuery
   extend ActiveModel::Naming
   
   attr_accessor :guests, :start_date, :end_date
-     
-  validates_presence_of :start_date, :end_date, :guests
-  validates :guests, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1 }
+       
+  validates :start_date, :end_date, :presence => true, :format => { :with => /^(19|20)\d\d-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])$/ }
+  validates :guests, :presence => true, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1 }
+  
+  validate :validate_end_date_after_start_date
   
   class << self
     def all
@@ -22,6 +24,12 @@ class AvailabilityQuery
 
   def persisted?
     false
+  end
+  
+  def validate_end_date_after_start_date
+    if start_date && end_date
+      errors.add(:start_date, "must fall before end date") if end_date < start_date
+    end
   end
 
 end
--- spec/models/availability_query_spec.rb
+++ spec/models/availability_query_spec.rb
@@ -8,9 +8,25 @@ describe AvailabilityQuery do
     it { should validate_presence_of(:end_date) }
     it { should validate_presence_of(:guests) }
     it { should validate_numericality_of(:guests).only_integer }
+    it { should validate_format_of(:start_date).with('2012-02-12') }
+    it { should validate_format_of(:end_date).with('2012-02-12') }    
     
     it "should not be valid if guests is less than 1" do
       b.guests = 0
+      b.should_not be_valid
+    end
+    
+    it "should not be valid if end date is before start date" do
+      temp = b.start_date
+      b.start_date = b.end_date
+      b.end_date = temp
+      b.should_not be_valid
+    end
+    
+    it "should not be valid if start date and/or end date are not in 'yyyy-mm-dd' format" do
+      b.start_date = '01-01-2012'
+      b.should_not be_valid
+      b.end_date = 'fgsagfsdafsa'
       b.should_not be_valid
     end
     
